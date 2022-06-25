cb38ae3f1f77a892072c7c9978c91d54a6d4c0ee
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-06-26 00:29:40 +0530

    Create 2_show_databases.sql

---
+++ 2_show_databases.sql
@@ -0,0 +1,16 @@
+SHOW DATABASES;
+
++--------------------+
+| Database           |
++--------------------+
+| covaxMS            |
+| day1               |
+| exam               |
+| information_schema |
+| lab                |
+| mysql              |
+| performance_schema |
+| practice           |    <----------------------------------
+| sys                |
++--------------------+
+9 rows in set (0.00 sec)
